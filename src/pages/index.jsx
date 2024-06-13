ae11427b846d629309721235ab1184fba76d228b
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2024-06-13 01:17:33 -0300

    sad

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,9 +7,8 @@ export default function Index() {
     <div className='w-full h-auto'>
     <div className='w-full h-screen'>
       <video  className='w-full relative h-screen object-cover' muted autoPlay  src="https://firebasestorage.googleapis.com/v0/b/boda-8ade5.appspot.com/o/Gus%20y%20Ari.mp4?alt=media&token=922f3180-dc4e-4fcf-a2ee-78decff8097d" ></video>
-    <div className='absolute top-[80%] left-[57%] flex gap-10'>
-      <Anchor className='bg-[red] px-[0.7rem] py-[0.3rem] text-center text-white font-semibold rounded-[10px]'>Ingresar con música</Anchor>
-      <Anchor to={'/templated'} className='bg-[red] px-[0.7rem] py-[0.3rem] text-center font-semibold text-white rounded-[10px]'>Ingresar sin música</Anchor>
+    <div className='absolute top-[80%] left-[65%] flex gap-10'>
+      <Anchor to={'/templated'} className='bg-[red] px-[0.7rem] py-[0.3rem] text-center font-semibold text-white rounded-[10px]'>Ver la invitacion!</Anchor>
     </div>
     </div>
     </div>
